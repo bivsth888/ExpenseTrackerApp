e4c84db1e90a826bddb9478018b12781116fbf80
bivsth88 <bivsth88@gmail.com>
2025-02-25 12:30:47 -0500

    Initial commit of ExpenseTrackerApp

---
+++ db_connection.py
@@ -0,0 +1,6 @@
+import pyodbc
+
+# Set up the connection string
+def get_db_connection():
+    connection_string = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=ExpenseTracker;Trusted_Connection=yes;"
+    return pyodbc.connect(connection_string)
